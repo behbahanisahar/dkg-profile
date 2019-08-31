f88a229e968e8b6e3506259c569c1b1fa67f14d4
sahar behbahani <s.behbahani71@gmail.com>
2019-08-31 14:32:29 +0430

    fix styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import App from './components/App';
 
